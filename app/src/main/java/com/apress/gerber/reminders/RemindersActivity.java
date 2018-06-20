197e9a689fea6b39dc01756a78b8f93466bb3546
qingche11 <13770604869@163.com>
2018-06-20 21:11:10 +0800

    third

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -14,6 +14,6 @@ public class RemindersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
-
+        //lll
     }
 }
